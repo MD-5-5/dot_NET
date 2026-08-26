b17eecf9c9bbd559539b94e5b1231384a3e0a6ed
Dev Srivastava <devsrivastav64@gmail.com>
2026-08-26 09:58:01 +0530

    Added Practice and PractiveWebPages

---
+++ Practice/Practice/Controllers/FirstController1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    public class FirstController1 : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(); 
+        }
+    }
+}
---
+++ PractiveWebPages/Controllers/CustomController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PractiveWebPages.Controllers
+{
+    public class CustomController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Location()
+        {
+            return View();
+        }
+
+        public IActionResult Ratings()
+        {
+            return View();
+        }
+    }
+}
