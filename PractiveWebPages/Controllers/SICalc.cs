62b00d74566604db70d6c9b32486be65c0e0b79a
Dev Srivastava <devsrivastav64@gmail.com>
2026-08-27 03:24:22 +0530

    Add simple interest calculator controller, model, and view
    Added SICalc controller and SI model for simple interest calculation. Updated project user settings for view scaffolder. Added Create.cshtml view with form for Principal, Rate, and Time inputs.

---
+++ PractiveWebPages/Controllers/SICalc.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PractiveWebPages.Models;
+namespace PractiveWebPages.Controllers
+{
+    public class SICalc : Controller
+    {
+        public IActionResult Index()
+        {
+            SI obj = new SI();
+            return View(obj);
+        }
+    }
+}
---
+++ PractiveWebPages/Models/SI.cs
@@ -0,0 +1,10 @@
+﻿namespace PractiveWebPages.Models
+{
+    public class SI
+    {
+        public double Principal { get; set; }
+        public double Rate { get; set; }
+        public double Time { get; set; }
+
+    }
+}
