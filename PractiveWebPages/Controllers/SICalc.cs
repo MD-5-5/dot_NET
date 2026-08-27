563891c519dd5076f84686f8cc615ad27123f6f6
Dev Srivastava <devsrivastav64@gmail.com>
2026-08-28 01:57:21 +0530

    ways to pass data from controller to views

--- PractiveWebPages/Controllers/SICalc.cs
+++ PractiveWebPages/Controllers/SICalc.cs
@@ -4,6 +4,7 @@ namespace PractiveWebPages.Controllers
 {
     public class SICalc : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             SI obj = new SI();
@@ -12,9 +13,11 @@ namespace PractiveWebPages.Controllers
         [HttpPost]
         public IActionResult Index(SI obj)
         {
-            double simpleInterest = (obj.Principal * obj.Rate * obj.Time) / 100;
-            ViewBag.SimpleInterest = "Result is: " + simpleInterest;
-            return View(obj);
+            double simpleInterest = (obj.Principal * obj.Rate * obj.Time) / 100; //getter
+            ViewBag.SimpleInterest = "Result is: " + simpleInterest; //ASP.NET MVC/Core MVC mein ViewBag ka kaam hota hai Controller se View (HTML/Razor page) ko temporary data bhejna.
+            return View(obj); //return View(obj) ka matlab hai ki humne jo object (obj) pass kiya hai, usko View mein bhej rahe hain. Isse View mein hum obj ke properties ko access kar sakte hain.
         }
+
+
     }
 }
