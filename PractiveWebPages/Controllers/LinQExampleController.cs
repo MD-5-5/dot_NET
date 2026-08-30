a77c568397835ce599859c6dd3ca5cae9031ec8d
Dev Srivastava <devsrivastav64@gmail.com>
2026-08-30 23:45:09 +0530

    LinQ Introduction

---
+++ PractiveWebPages/Controllers/LinQExampleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PractiveWebPages.Controllers
+{
+    public class LinQExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            String[] names = { "John", "Jane", "Alice", "Bob", "Charlie" };
+            var s = names.Where(n => n.Length > 3);
+            ViewBag.Names = s;
+            return View();
+        }
+    }
+}
